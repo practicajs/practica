630d6435f11045d4ea199e2c1f4ff304ae6e06c0
Yoni <me@goldbergyoni.com>
2022-03-22 11:21:21 +0200

    E2E tests pass

--- .dist/libraries/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// This is not a real logger as its just writes to the console
-// but it has the structure of a real logger
-module.exports.info = function (message) {
-    console.log(message);
-};
-module.exports.error = function (message) {
-    console.error(message);
-};
