20878fdcec5a357075db4c1f9b5913956a43e272
Yoni <me@goldbergyoni.com>
2022-03-28 18:35:54 +0300

    Genric message

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { startAppGenerator } from "../src/code-generator";
+import { startAppGenerator } from "../src/code-generator/entry-points/cli-entry-point";
 import packageJSON from "../package.json";
 
 startAppGenerator();
---
+++ src/code-generator/entry-points/cli-entry-point.ts
@@ -0,0 +1,33 @@
+const { program } = require("commander");
+import { renderWizard } from "./interactive-cli";
+const { handleNonInteractiveCommand } = require("./non-interactive-cli");
+
+export function startAppGenerator() {
+  console.log("Starting app generator");
+  program
+    .name("Practice - Best Practices Generator")
+    .description("Generate best practices for your project")
+    .version("0.0.1");
+
+  program
+    .command("interactive")
+    .description("Open an interactive UI for customization")
+    .action((str, options) => {
+      renderWizard();
+    });
+
+  program
+    .command("generate")
+    .description("Generates code using flags (a non-interactive CLI)")
+    .option("-f, --framework <string>", "Framework to use")
+    .option("-d, --db <string>", "DB to use")
+    .option("-id, --install-dependencies", "Whether to install dependencies")
+    .action((options) => {
+      handleNonInteractiveCommand(options);
+    });
+
+  program.option("--framework", "Use framework");
+  program.option("--db", "Use DB");
+
+  program.parse();
+}
