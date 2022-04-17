e086c86e504dc3f3c4f6a0543d75a6ccb152fb5d
ronen <roncho1794@gmail.com>
2022-04-17 17:45:27 +0300

    init health service draft - library

--- src/code-templates/libraries/health/health.ts
+++ src/code-templates/libraries/health/health.ts
@@ -3,17 +3,30 @@ const defaultOptions = {
     readinessPath: "/readiness"
 };
 
+
 // Should be implemented differently for SQL / Mongo? Express / Fastify / nest ?
 export const HealthService = {
     init(baseRoute, app, dbConnection) { // TODO - should receive as params: app to register routes, connection to db
+
+        process.on('SIGTERM', () => {
+
+        })
+
         const livenessRoute = baseRoute + defaultOptions.livenessPath
         const readinessRoute = baseRoute + defaultOptions.readinessPath
+
         /* Liveness */
         app.get(livenessRoute, (request, response) => {
+            const data = {
+                uptime: process.uptime(),
+                message: 'OK',
+                date: new Date()
+            }
             response
                 .status(200)
-                .end();
+                .send(data)
         })
+
         /* Readiness */
         app.get(readinessRoute, (request, response) => {
             // TODO - check DB connection
@@ -21,5 +34,6 @@ export const HealthService = {
                 .status(200)
                 .end();
         })
+
     },
 }
