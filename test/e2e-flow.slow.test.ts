2fdf51332f87c9bdc9bdb20b6417b8a5bbe80c2c
Yoni <me@goldbergyoni.com>
2022-04-26 18:11:08 +0300

    Improved E2E

--- test/e2e-flow.slow.test.ts
+++ test/e2e-flow.slow.test.ts
@@ -24,7 +24,7 @@ describe("Non-interactive", () => {
     // Act
     await execa("generate", ["generate", "--install-dependencies"], {
       cwd: emptyFolderForATest,
-    });
+    });ls
 
     // Assert
     const testResult = await execa("npm", ["test"], {
