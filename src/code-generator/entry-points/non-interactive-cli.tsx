38d98e3f3717eb5c99a01461459d3aed49267ec5
Jose Alvarez <joseluh_1@hotmail.com>
2022-05-03 12:29:47 -0500

    fix: typo on non-interactive-cli.tsx

--- src/code-generator/entry-points/non-interactive-cli.tsx
+++ src/code-generator/entry-points/non-interactive-cli.tsx
@@ -6,7 +6,7 @@ const generateService = require("../generation-logic/generate-service");
 export function handleNonInteractiveCommand(options: any) {
   const generationOptions = factorDefaultOptions({
     installDependencies: options.installDependencies,
-    overridePrevious: options.overrideIfExists,
+    overrideIfExists: options.overrideIfExists,
     targetDirectory: process.cwd(),
   });
   generateService.generateApp(generationOptions); //TODO: Make this async, show progress bar and nice message in the end
