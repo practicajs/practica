39c16d3b9e214ee7ba09b3c1af432937e1c08fc6
roncho <roncho1794@gmail.com>
2022-05-14 21:16:24 +0300

    error handler - v1

--- src/code-templates/libraries/error-handling/error-handling.ts
+++ src/code-templates/libraries/error-handling/error-handling.ts
@@ -1,34 +1,79 @@
+import * as Http from "http";
 const logger = require("../../libraries/logger/logger");
+
+let httpServerRef: Http.Server
 
 // This file simulates real-world error handler that makes this component observable
 const errorHandler = {
-  handleError: async (errorToHandle) => {
-    try {
-      logger.error(errorToHandle);
-      metricsExporter.fireMetric("error", {
-        errorName: errorToHandle.name || "generic-error",
-      });
 
-      // A common best practice is to crash when an unknown error (non-trusted) is being thrown
-      decideWhetherToCrash(errorToHandle);
-    } catch (e) {
-      // Continue the code flow if failed to handle the error
-      console.log(`handleError threw an error ${e}`);
-    }
+  // Listen to the error events which won't be handled by programmer
+  listenToErrorEvents: (httpServer: Http.Server, options) => {
+    httpServerRef = httpServer
+    process.on('uncaughtException', async error => {
+      await errorHandler.handleError(error)
+    })
+
+    process.on('unhandledRejection', async reason => {
+      await errorHandler.handleError(reason)
+    })
+
+    process.on('SIGTERM', async () => {
+      logger.error("App received SIGTERM event, try to gracefully close the server")
+      await terminateHttpServer()
+    })
+
+    process.on('SIGINT', async () => {
+      logger.error("App received SIGINT event, try to gracefully close the server")
+      await terminateHttpServer()
+    })
   },
-};
 
-const decideWhetherToCrash = (error) => {
-  if (!error.isTrusted) {
-    process.exit();
+  handleError: (errorToHandle: any) => {
+    try {
+      const appError: AppError = normalizeError(errorToHandle)
+      logger.error(appError);
+      metricsExporter.fireMetric("error", { errorName: appError.name });
+      // A common best practice is to crash when an unknown error (non-trusted) is being thrown
+      if (!appError.isTrusted) {
+        terminateHttpServer()
+      }
+    } catch (e) {
+      logger.error("Error Handler failed to handleError properly")
+      logger.error(e)
+      // Should we crash here?
+    }
   }
 };
 
+const terminateHttpServer = async () => {
+    // maybe implement more complex logic here (like using 'http-terminator' library)
+    if (httpServerRef) {
+      await httpServerRef.close()
+    }
+    process.exit()
+}
+
+
+// The input might won't be 'AppError' or even 'Error' instance, the output of this function will be - AppError.
+const normalizeError = (errorToHandle: any): AppError => {
+  if (errorToHandle instanceof AppError) {
+    return errorToHandle
+  }
+  if (errorToHandle instanceof Error) {
+    return new AppError(errorToHandle.name, errorToHandle.message)
+  }
+  // meaning it could e any type,
+  const inputType = typeof errorToHandle
+  return new AppError('general-error', `Error Handler received a none error instance with type - ${inputType}, value - ${errorToHandle}`)
+}
+
+
 class AppError extends Error {
-  constructor(name, message, public HTTPStatus, public isTrusted) {
+  constructor(name, message, public HTTPStatus?, public isTrusted = true) {
     super(message);
     this.name = name;
-    this.isTrusted = isTrusted === undefined ? true : false;
+    this.HTTPStatus = HTTPStatus;
+    this.isTrusted = isTrusted;
   }
 }
 
---
+++ src/code-templates/libraries/error-handling/tests/error-handler.test.ts
@@ -0,0 +1,3 @@
+describe('Error Handler', () => {
+    test('', () => {})
+})
