4fff855062f031c63d83a4c6064f87748acdeedd
Daniel Gluskin <glusking@gmail.com>
2022-06-25 23:48:49 +0300

    fix typo

--- src/code-templates/.eslintrc.js
+++ src/code-templates/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: '13`',
+    ecmaVersion: '2021',
     sourceType: 'module',
   },
   plugins: [
