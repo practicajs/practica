02e0965b2f955107f2abb57e4dc5e6142411d3c1
Michael Solomon <micheal540@gmail.com>
2022-08-09 21:16:16 +0300

    fix

--- src/code-templates/services/order-service/domain/payment-terms-service.ts
+++ src/code-templates/services/order-service/domain/payment-terms-service.ts
@@ -1,10 +1,8 @@
 // ️️️✅ Best Practice: Use services for scoped and specific pieces of business logic
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function determinePaymentTerms(requestedTerms: number, userId: number) {
   // In real-world app, more logic and even integrations will come here
-  if (userId > 10) {
-    return 60;
-  }
-  return requestedTerms;
+  return 60;
 }
 
 export default { determinePaymentTerms };
