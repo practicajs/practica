474d0348ea4a4deea98ce8bff686c50c0a3970f5
Michael Solomon <micheal540@gmail.com>
2022-09-04 22:55:23 +0300

    fix

--- src/code-templates/libraries/validation/ajv-cache.ts
+++ src/code-templates/libraries/validation/ajv-cache.ts
@@ -1,6 +1,7 @@
 import AJV from 'ajv';
 
-// This file is used to provide a 'singleton' AJV instance for the entire app so // every client can cache the compiled schemas
+// This file is used to provide a 'singleton' AJV instance for the entire app so
+// every client can cache the compiled schemas
 
 const ajv = new AJV();
 ajv.addKeyword('kind');
