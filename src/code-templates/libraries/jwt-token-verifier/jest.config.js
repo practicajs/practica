63faad45f79f56d874d7b2e0b163acf6b445feaf
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2022-09-16 12:20:56 +0300

    fix eslint error

--- src/code-templates/libraries/jwt-token-verifier/jest.config.js
+++ src/code-templates/libraries/jwt-token-verifier/jest.config.js
@@ -128,7 +128,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['sinon-jest-matchers/all'],
+  setupFilesAfterEnv: ['sinon-jest-matchers/all', './test/setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
