9916351635caa99ffb3d47c3bf86907721b16bd9
Yoni Goldberg <goldbergyoni@Yonis-MacBook-Pro-2.local>
2022-11-08 13:57:36 +0200

    The chosen app name is now being respected

--- too/libraries/jwt-token-verifier/package/lib/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './jwt-verifier-middleware';
--- too/services/order-service/domain/payment-terms-service.ts
+++
@@ -1,7 +0,0 @@
-// ️️️✅ Best Practice: Use services for scoped and specific pieces of business logic
-function determinePaymentTerms(requestedTerms: number, userId: number) {
-  // In real-world app, more logic and even integrations will come here
-  return 30 || requestedTerms + userId;
-}
-
-export default { determinePaymentTerms };
