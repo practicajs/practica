113ec3539ea8ba9e138b75b6d6062b258de3541c
Yoni Goldberg <goldbergyoni@Yonis-MacBook-Pro-2.local>
2022-11-16 15:42:35 +0200

    Prisma client to its default location

--- src/code-templates/services/order-service/entry-points/api/routes.ts
+++ src/code-templates/services/order-service/entry-points/api/routes.ts
@@ -16,6 +16,7 @@ export default function defineRoutes(expressApp: express.Application) {
       return res.json(addOrderResponse);
     } catch (error) {
       next(error);
+      return undefined;
     }
   });
 
